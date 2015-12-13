4c5c94034b3b445548a817279f98c29ce593909d
Dave Abel <dave.abel.cs@gmail.com>
2015-12-13 15:40:00 -0500

    updated plots with better formatting

---
+++ plots/python/dave_plotting.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 50 Trajectories
+
+fiftyTrajData = [7, 13, 12, 4, 6, 7, 4]
+twoHundredTrajData = [7, 10, 10, 10,  8, 7, 9]
+thousandTrajData = [8, 10, 10, 10, 10, 10, 10]
+
+xData = [2, 4, 6, 8, 10, 15, 20]
+
+plt.plot(xData, thousandTrajData, label="1000", marker="x", color="red")
+plt.plot(xData, twoHundredTrajData, label="200", marker="8", color="green")
+plt.plot(xData, fiftyTrajData, label="50", marker="s", color="blue")
+
+plt.legend(title="Num Trajectories")
+
+
+plt.xlim([0,20])
+plt.ylim([0,16])
+plt.xlabel('Planning Depth')
+plt.ylabel('Average Cumulative Reward')
+plt.title('Performance of UCT vs. Planning Depth')
+plt.grid(True)
+plt.savefig("test.png")
+plt.show()
